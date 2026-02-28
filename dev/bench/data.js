fd8a449e95cc20d9a26f9e7f01c6df0fc971d47a
github-action-benchmark <github@users.noreply.github.com>
2026-02-28 04:07:35 +0000

    add Benchmark (cargo) benchmark result for 36b23b7a0c1ac12f7cbf381393ce5a98c4c17180

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772166414612,
+  "lastUpdate": 1772251655624,
   "repoUrl": "https://github.com/ahrav/scratch-scanner-rs",
   "entries": {
     "Benchmark": [
@@ -3700,6 +3700,334 @@ window.BENCHMARK_DATA = {
           {
             "name": "offline_validate_slack_token/invalid_segments",
             "value": 10,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "offline_validate_slack_token/unknown_prefix",
+            "value": 5,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "ahrav",
+            "username": "ahrav",
+            "email": "ahravdutta02@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "36b23b7a0c1ac12f7cbf381393ce5a98c4c17180",
+          "message": "Merge pull request #156 from ahrav/feature/env-parsing\n\nAdd dotenv extractor for structured .env file scanning",
+          "timestamp": "2026-02-27T23:49:11Z",
+          "url": "https://github.com/ahrav/scratch-scanner-rs/commit/36b23b7a0c1ac12f7cbf381393ce5a98c4c17180"
+        },
+        "date": 1772251654830,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "write_u64/value/small_42",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write_u64/value/medium_1_7B",
+            "value": 8,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write_u64/value/large_u64_max",
+            "value": 13,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write_oid_hex/sha1_20B",
+            "value": 7,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write_oid_hex/sha256_32B",
+            "value": 12,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write_json_str/scalar_short/4",
+            "value": 6,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write_json_str/scalar_short/8",
+            "value": 7,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write_json_str/scalar_short/14",
+            "value": 11,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write_json_str/clean_ascii/32",
+            "value": 6,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write_json_str/clean_ascii/128",
+            "value": 14,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write_json_str/clean_ascii/512",
+            "value": 55,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write_json_str/with_escapes/32",
+            "value": 42,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write_json_str/with_escapes/128",
+            "value": 108,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write_json_str/with_escapes/512",
+            "value": 391,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write_json_str/sparse_escapes/512",
+            "value": 72,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write_json_bytes/clean_ascii_128B",
+            "value": 11,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write_json_bytes/with_0xff_128B",
+            "value": 316,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write_f64/81.23",
+            "value": 18,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "encode_finding/fs_finding",
+            "value": 48,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "encode_finding/git_finding",
+            "value": 67,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "encode_commit_meta/no_identity",
+            "value": 22,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "encode_commit_meta/with_identity",
+            "value": 42,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "encode_batch/1000_findings",
+            "value": 59343,
+            "range": "± 257",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sink_emit/1000_findings_dev_null",
+            "value": 72092,
+            "range": "± 356",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "json_sink_emit/1000_findings_dev_null",
+            "value": 78487,
+            "range": "± 323",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sink_contention/threads/2",
+            "value": 203955,
+            "range": "± 3922",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sink_contention/threads/4",
+            "value": 191877,
+            "range": "± 1799",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sink_contention/threads/8",
+            "value": 225563,
+            "range": "± 3491",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "u64_vs_itoa/custom_write_u64",
+            "value": 8,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "u64_vs_itoa/itoa_write",
+            "value": 8,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "f64_vs_ryu/custom_write_f64",
+            "value": 18,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "f64_vs_ryu/ryu_write",
+            "value": 49,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "offline_validate_aws_access_key/valid_akia",
+            "value": 29,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "offline_validate_aws_access_key/valid_a3t",
+            "value": 29,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "offline_validate_aws_access_key/invalid_charset",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "offline_validate_aws_access_key/invalid_prefix",
+            "value": 4,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "offline_validate_sentry_org_token/valid_small",
+            "value": 67,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "offline_validate_sentry_org_token/valid_large",
+            "value": 299,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "offline_validate_sentry_org_token/invalid_prefix",
+            "value": 288,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "offline_validate_sentry_org_token/invalid_char",
+            "value": 280,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "offline_validate_sentry_org_token/oversized_payload",
+            "value": 38,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "offline_validate_pypi_token/valid",
+            "value": 22,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "offline_validate_pypi_token/wrong_header",
+            "value": 22,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "offline_validate_pypi_token/invalid_char",
+            "value": 22,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "offline_validate_pypi_token/too_short",
+            "value": 0,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "offline_validate_slack_token/bot_current",
+            "value": 47,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "offline_validate_slack_token/bot_legacy",
+            "value": 31,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "offline_validate_slack_token/user_token",
+            "value": 66,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "offline_validate_slack_token/config_access",
+            "value": 118,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "offline_validate_slack_token/invalid_segments",
+            "value": 9,
             "range": "± 0",
             "unit": "ns/iter"
           },
